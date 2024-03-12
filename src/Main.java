c3bdca017c603f02d455ccbb5c414cad126714ec
evans-owa <106734230+evans-owa@users.noreply.github.com>
2024-03-12 22:00:44 +0300

    java max

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        //2d array
+        int[] number;
+        number  = new   int[5];
+
+        // 2d declaration of an array
+        int[][] numbers = new int[3][5];
+
+//        accessing the element using the row and column
+        //using the index reference
+        //numbers[1][3]
+
+
+        // assign example we the array using the row and the columns
+
+        numbers[0][0] = 6;
+        numbers[0][1] = 65;
+        numbers[0][2] = 3;
+        numbers[0][3] = 2;
+        numbers[0][4] = 8;
+        numbers[1][0] = 6;
+        numbers[1][1] = 7;
+        numbers[1][2] = 4;
+        numbers[1][3] = 22;
+    }
+}
